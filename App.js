365af5a18265348a533c8d3804596a4d6258aba2
RED Academy <redacademy@REDs-MacBook-Air.local>
2019-03-13 10:51:10 -0400

    feat: created color palette

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation'
 import { ApolloProvider } from 'react-apollo'
+import { ThemeContext, getTheme } from 'react-native-material-ui';
 
 import client from './src/config/Api'
 import AppNavigator from './src/AppNavigator'
@@ -8,11 +9,24 @@ import AppNavigator from './src/AppNavigator'
 const AppContainer = createAppContainer(AppNavigator)
 
 
+const uiTheme = {
+  palette: {
+    blackColor: '#000000',
+    mediumGreyColor: '#999999',
+    lightGreyColor: '#e6e6e6',
+    blueColor: '#8797D6',
+    purpleColor: '#9963ea',
+    redColor: '#cf392a'
+  },
+};
+
 const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <AppContainer/>
-    </ApolloProvider>
+    <ThemeContext.Provider value={getTheme(uiTheme)}>
+      <ApolloProvider client={client}>
+        <AppContainer/>
+      </ApolloProvider>
+    </ThemeContext.Provider>
   )
 }
 
