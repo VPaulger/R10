f2eb63445692ae51d27269e54772bc8fcafad788
App Dev 1 <appdev1@REDs-MacBook-Air.local>
2019-03-15 09:34:03 -0400

    Added the functionality for the favourites page

--- src/Faves.js
+++ src/Faves.js
@@ -1,18 +1,100 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Link,
+  AsyncStorage,
+} from 'react-native';
 import FavouriteIcon from './FavouriteIcon';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { ScrollView } from 'react-native-gesture-handler';
+import moment from 'moment';
 
 class Faves extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      favList: null,
+    };
+  }
+
+  async getFavList() {
+    try {
+      const favList = await AsyncStorage.getItem('favList');
+      this.setState({ favList });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  isFav(id) {
+    const getFavListString = this.state.favList;
+    const getFavListJSON = JSON.parse(getFavListString) || {};
+    const isFav = getFavListJSON[id] != null && getFavListJSON[id] != false;
+    console.log('Is Fav: ', isFav);
+    console.log('favListVal: ', getFavListJSON[id]);
+    return isFav;
+  }
+
+  componentDidMount() {
+    this.getFavList();
+  }
+
+  componentDidUpdate() {
+    this.getFavList();
   }
 
   render() {
+    const { navigate } = this.props.navigation;
+    let lastTime;
     return (
       <View style={styles.container}>
-        <Text>Faves</Text>
-        <Text>This is the faves page :)</Text>
-        <FavouriteIcon id="cjh2j37mo163p01221qpcklry" />
+        <ScrollView>
+          {/* <Button 
+          title="Go to the session page"
+          onPress={() => navigate('SessionPage')}
+        /> */}
+
+          <Query query={GET_SESSIONS}>
+            {({ loading, data }) => {
+              if (loading || !data) return <Text>LOADING....</Text>;
+              return (
+                <View style={styles.list}>
+                  {console.log('DATA>>>', data.allSessions[0])}
+                  {data.allSessions.map(session => {
+                    if (this.isFav(session.id)) {
+                      const showSessionTime = lastTime !== session.startTime;
+                      lastTime = session.startTime;
+                      return (
+                        <View>
+                          {showSessionTime && (
+                            <Text style={styles.startTime}>
+                              {moment(session.startTime).format('LT')}
+                            </Text>
+                          )}
+                          <Text style={styles.title}>{session.title}</Text>
+                          <View style={styles.scheduleContainer}>
+                            <Text style={styles.location}>
+                              {session.location}
+                            </Text>
+                            <FavouriteIcon
+                              id={session.id}
+                              styles={styles.favIcon}
+                            />
+                          </View>
+                          {/* </Link> */}
+                        </View>
+                      );
+                    }
+                  })}
+                </View>
+              );
+            }}
+          </Query>
+        </ScrollView>
       </View>
     );
   }
@@ -20,11 +102,55 @@ class Faves extends Component {
 
 export default Faves;
 
+const GET_SESSIONS = gql`
+  query {
+    allSessions(orderBy: startTime_ASC) {
+      id
+      title
+      startTime
+      location
+      description
+      speaker {
+        id
+      }
+    }
+  }
+`;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  scheduleContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+  },
+  favIcon: {
+    backgroundColor: '#FFFFFF',
+    paddingRight: 10,
+  },
+  list: {
+    width: '100%',
+  },
+  startTime: {
+    backgroundColor: '#e6e6e6',
+    padding: 10,
+  },
+  title: {
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+    paddingTop: 15,
+  },
+  location: {
+    backgroundColor: '#FFFFFF',
+    color: '#999999',
+    padding: 10,
+    paddingTop: 0,
+    paddingBottom: 15,
   },
 });
